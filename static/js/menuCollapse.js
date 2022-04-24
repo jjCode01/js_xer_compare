41f78fab7ad93df49044a7f1ec4c4d3fb6d8ce0f
Jesse Jones <jessej82@gmail.com>
2022-04-24 06:24:26 -0700

    deleted unused files

--- static/js/menuCollapse.js
+++
@@ -1,9 +0,0 @@
-const coll = document.querySelectorAll(".collapsible");
-coll.forEach(el => {
-    el.addEventListener("click", function() {
-        const content = this.nextElementSibling;
-        content.style.display = content.style.display === "none" ? "initial" : "none"
-        this.classList.toggle("exp")
-        this.classList.toggle("ctr")
-    });
-})
