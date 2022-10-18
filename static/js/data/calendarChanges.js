bcdcf22dda139e0a8ff6796959abc2dd1ebe5599
Jesse Jones <code@seqmanagement.com>
2022-10-17 20:24:46 -0700

    fixed missing column in deleted calendar table

--- static/js/data/calendarChanges.js
+++ static/js/data/calendarChanges.js
@@ -16,7 +16,7 @@ export let calendarChanges = {
         ['Calendar Name', 'Type', 'Assignments', 'Sun Hrs', 'Mon Hrs', 'Tue Hrs', 'Wed Hrs', 'Thu Hrs', 'Fri Hrs', 'Sat Hrs'],
         function() {
             return this.data.map(cal => [
-                cal.clndr_name, cal.type, , cal.week[0].hours, cal.week[1].hours, cal.week[2].hours, cal.week[3].hours, cal.week[4].hours, cal.week[5].hours, cal.week[6].hours 
+                cal.clndr_name, cal.type, cal.assignments, cal.week[0].hours, cal.week[1].hours, cal.week[2].hours, cal.week[3].hours, cal.week[4].hours, cal.week[5].hours, cal.week[6].hours 
             ])
         }
     ),
